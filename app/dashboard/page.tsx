93c518c7edf44848d43e45bc25e1bfb4a02987ce
Mongkul <105968456+MongkulSokha@users.noreply.github.com>
2025-10-01 14:11:29 +0700

    fixed dashboard route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,41 +3,35 @@ import TableData from "@/components/dashboard/table-data";
 import { Button } from "@/components/ui/button";
 import { IconPlus } from "@tabler/icons-react";
 import Link from "next/link";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import ProtectedLayout from "./ProtectedLayout";
 
 export default async function Page() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) redirect("/signin");
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <SidebarInset>
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 p-5">
-              <Link href="/dashboard/add">
-                <Button variant="outline" size="sm">
-                  <IconPlus />
-                  <span className="hidden lg:inline">Add Card</span>
-                </Button>
-              </Link>
-              <TableData />
+    <ProtectedLayout>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <SidebarInset>
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 p-5">
+                <Link href="/dashboard/add">
+                  <Button variant="outline" size="sm">
+                    <IconPlus />
+                    <span className="hidden lg:inline">Add Card</span>
+                  </Button>
+                </Link>
+                <TableData />
+              </div>
             </div>
           </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+        </SidebarInset>
+      </SidebarProvider>
+    </ProtectedLayout>
   );
-}
-function auth() {
-  throw new Error("Function not implemented.");
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,46 +13,46 @@ import Gallerys from "@/components/gallery";
 import CardList from "@/components/cardlist";
 import ProtectedLayout from "./dashboard/ProtectedLayout";
 
+export const dynamic = "force-dynamic"; // important!
+
 export default async function Home() {
   return (
-    <ProtectedLayout>
-      <main className="App">
-        <Navbar />
-        <Corousel />
-        <Welcome />
-        <CardSlider />
-        <WhyChooseUs />
-        <Counter />
-        {/* Upcoming Events */}
-        <div>
-          <div className="flex pt-20 px-20 flex-col text-center items-center">
-            <p className="text-2xl font-bold uppercase">Upcoming Events</p>
-            <div className="flex items-center h-[0.2rem] bg-red-900 mt-5 w-45"></div>
-          </div>
-          <CardList />
+    <main className="App">
+      <Navbar />
+      <Corousel />
+      <Welcome />
+      <CardSlider />
+      <WhyChooseUs />
+      <Counter />
+      {/* Upcoming Events */}
+      <div>
+        <div className="flex pt-20 px-20 flex-col text-center items-center">
+          <p className="text-2xl font-bold uppercase">Upcoming Events</p>
+          <div className="flex items-center h-[0.2rem] bg-red-900 mt-5 w-45"></div>
         </div>
-        <CurriculumSlider />
-        <Gallerys />
-        {/* Testimonial */}
-        <div>
-          <div className="px-20 md:px-20 xl:px-60 bg-gray-100 h-full flex pt-20 px-20 flex-col text-center items-center">
-            <p className="text-2xl font-bold uppercase">Testimonial</p>
-            <div className="flex items-center h-[0.2rem] bg-red-900 mt-3 w-35"></div>
-            <div className="p-5 w-full mx-20 md:mx-20 xl:mx-60 my-20 flex overflow-x-auto justify-between gap-6 md:gap-5">
-              {testimonialData.map((card) => (
-                <TestimonialCard
-                  key={card.id}
-                  name={card.name}
-                  img={card.img}
-                  position={card.position}
-                  text={card.text}
-                />
-              ))}
-            </div>
+        <CardList />
+      </div>
+      <CurriculumSlider />
+      <Gallerys />
+      {/* Testimonial */}
+      <div>
+        <div className="px-20 md:px-20 xl:px-60 bg-gray-100 h-full flex pt-20 px-20 flex-col text-center items-center">
+          <p className="text-2xl font-bold uppercase">Testimonial</p>
+          <div className="flex items-center h-[0.2rem] bg-red-900 mt-3 w-35"></div>
+          <div className="p-5 w-full mx-20 md:mx-20 xl:mx-60 my-20 flex overflow-x-auto justify-between gap-6 md:gap-5">
+            {testimonialData.map((card) => (
+              <TestimonialCard
+                key={card.id}
+                name={card.name}
+                img={card.img}
+                position={card.position}
+                text={card.text}
+              />
+            ))}
           </div>
         </div>
-        <Footers />
-      </main>
-    </ProtectedLayout>
+      </div>
+      <Footers />
+    </main>
   );
 }
